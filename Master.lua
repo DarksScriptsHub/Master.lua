da2ceda9e17a009435842c8acbbcc6132a96a7c5
DarksScriptsHub <ohiooriginalxd@gmail.com>
2025-06-30 05:03:37 -0600

    Update Master.lua

--- Master.lua
+++ Master.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/bgBk7piN"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/qWZvtdRX"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DarksScriptsHub/Grow.A.Garden.Duper.Darks.Scripts/refs/heads/main/7c22ff60a2ca037d53806a69f79e49bf.txt"))()
